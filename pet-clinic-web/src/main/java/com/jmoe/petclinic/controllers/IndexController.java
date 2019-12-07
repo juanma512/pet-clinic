db7ba1d2d7e332b109ec8837d8b947cf2fba84a7
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2019-12-07 13:50:55 +0100

    Added index controller and page. Closes #8.

---
+++ pet-clinic-web/src/main/java/com/jmoe/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.jmoe.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
