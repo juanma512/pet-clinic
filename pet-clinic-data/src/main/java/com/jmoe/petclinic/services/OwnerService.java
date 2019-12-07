346e9f85567564a488f1160df138816c6a4bfe58
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2019-12-07 12:39:00 +0100

    Added common crud service interface. Closes #11.

--- pet-clinic-data/src/main/java/com/jmoe/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/jmoe/petclinic/services/OwnerService.java
@@ -1,16 +1,9 @@
 package com.jmoe.petclinic.services;
 
 import com.jmoe.petclinic.model.Owner;
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
   Owner findByLastName(String lastName);
-
-  Owner findById(Long id);
-
-  Owner save(Owner owner);
-
-  Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/jmoe/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/jmoe/petclinic/services/PetService.java
@@ -1,14 +1,7 @@
 package com.jmoe.petclinic.services;
 
 import com.jmoe.petclinic.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-
-  Pet findById(Long id);
-
-  Pet save(Pet pet);
-
-  Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
