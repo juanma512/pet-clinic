e619f9bdeccc8cf74c76a3b3aad61476419e4fb3
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2019-12-09 21:19:34 +0100

    List owners. Closes #16.

--- pet-clinic-web/src/main/java/com/jmoe/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/jmoe/petclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.jmoe.petclinic.controllers;
 
+import com.jmoe.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "/owners/index";
     }
 
