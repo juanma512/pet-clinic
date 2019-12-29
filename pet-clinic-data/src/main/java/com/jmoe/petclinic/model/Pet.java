5e7ca38e3627c8b6470903666c870ed9ba1f56fb
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2019-12-29 16:51:25 +0100

    Added speciality entity. Closes #27.

--- pet-clinic-data/src/main/java/com/jmoe/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/jmoe/petclinic/model/Pet.java
@@ -2,17 +2,17 @@ package com.jmoe.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity {
+    public class Pet extends BaseEntity {
 
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
+        private PetType petType;
+        private Owner owner;
+        private LocalDate birthDate;
 
-    public PetType getPetType() {
-        return petType;
-    }
+        public PetType getPetType() {
+            return petType;
+        }
 
-    public void setPetType(PetType petType) {
+        public void setPetType(PetType petType) {
         this.petType = petType;
     }
 
--- pet-clinic-data/src/main/java/com/jmoe/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jmoe/petclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.jmoe.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
