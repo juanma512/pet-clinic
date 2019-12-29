4a67e08498420436f899963d72cbfdbc3f9db938
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2019-12-29 17:18:49 +0100

    Added pet types on startup. Closes #32.

--- pet-clinic-web/src/main/java/com/jmoe/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jmoe/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.jmoe.petclinic.bootstrap;
 
 import com.jmoe.petclinic.model.Owner;
+import com.jmoe.petclinic.model.PetType;
 import com.jmoe.petclinic.model.Vet;
 import com.jmoe.petclinic.services.OwnerService;
+import com.jmoe.petclinic.services.PetTypeService;
 import com.jmoe.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,41 +14,50 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,
+        PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+    }
+
+    private void createOwner(String firstName, String lastName) {
+        Owner owner = new Owner();
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+
+        ownerService.save(owner);
+    }
+
+    private void createVet(String firstName, String lastName) {
+        Vet vet = new Vet();
+        vet.setFirstName(firstName);
+        vet.setLastName(lastName);
+
+        vetService.save(vet);
+    }
+
+    private PetType createPetType(String name) {
+        PetType petType = new PetType();
+        petType.setName(name);
+
+        return petTypeService.save(petType);
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Juan Manuel");
-        owner1.setLastName("Oviedo");
+        PetType dog = createPetType("Dog");
+        PetType cat = createPetType("Cat");
 
-        ownerService.save(owner1);
-
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Mireia");
-        owner2.setLastName("Romero Moreno");
-
-        ownerService.save(owner2);
-
+        createOwner("Juan Manuel", "Oviedo");
+        createOwner("Mireia", "Romero Moreno");
         System.out.println("Loaded owners ...");
 
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Ramiro");
-        vet1.setLastName("Moreno Romero");
-
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Alicia");
-        vet2.setLastName("Garcia Ramirez");
-
-        vetService.save(vet2);
-
+        createVet("Ramiro", "Moreno Romero");
+        createVet("Alicia", "Garcia Ramirez");
         System.out.println("Loaded vets ...");
     }
 }
