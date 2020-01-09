f9658b77277c6004762f9ea632a7e64ac5560612
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2020-01-09 20:37:25 +0100

    Base entity as JPA. Closes #35

--- pet-clinic-data/src/main/java/com/jmoe/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/jmoe/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.jmoe.petclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
