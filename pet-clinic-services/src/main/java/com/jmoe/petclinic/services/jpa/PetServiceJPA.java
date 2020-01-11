6379e5a0241851b984ac78ad1a3a8398ccaea4e1
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2020-01-11 18:38:12 +0100

    Add Pet and PetType Services JPA. Closes #42

---
+++ pet-clinic-services/src/main/java/com/jmoe/petclinic/services/jpa/PetServiceJPA.java
@@ -0,0 +1,47 @@
+package com.jmoe.petclinic.services.jpa;
+
+import com.jmoe.petclinic.model.Pet;
+import com.jmoe.petclinic.repositories.PetRepository;
+import com.jmoe.petclinic.services.PetService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class PetServiceJPA implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
---
+++ pet-clinic-services/src/main/java/com/jmoe/petclinic/services/jpa/PetTypeServiceJPA.java
@@ -0,0 +1,47 @@
+package com.jmoe.petclinic.services.jpa;
+
+import com.jmoe.petclinic.model.PetType;
+import com.jmoe.petclinic.repositories.PetTypeRepository;
+import com.jmoe.petclinic.services.PetTypeService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeServiceJPA implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceJPA(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.findById(aLong);
+    }
+}
