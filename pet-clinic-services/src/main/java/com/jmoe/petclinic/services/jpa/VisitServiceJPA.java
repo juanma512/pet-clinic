f9b7c9ff0b0c8059d38e3325fc2ebc8763f6740d
Juan Manuel Oviedo Exposito <juanma512@gmail.com>
2020-01-11 19:06:56 +0100

    Add Visit Service JPA. Closes #46

---
+++ pet-clinic-services/src/main/java/com/jmoe/petclinic/services/jpa/VisitServiceJPA.java
@@ -0,0 +1,47 @@
+package com.jmoe.petclinic.services.jpa;
+
+import com.jmoe.petclinic.model.Visit;
+import com.jmoe.petclinic.repositories.VisitRepository;
+import com.jmoe.petclinic.services.VisitService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class VisitServiceJPA implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJPA(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
