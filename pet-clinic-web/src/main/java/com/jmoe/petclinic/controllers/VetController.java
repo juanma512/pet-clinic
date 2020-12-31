c2bc2a677b38c0e12514e1801621a4f6e6dd0986
Juan Manuel Oviedo Exposito <juan.manuel.oviedo.exposito@ing.com>
2020-12-31 14:01:40 +0100

    Closes #59. Vets json endpoint

--- pet-clinic-web/src/main/java/com/jmoe/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jmoe/petclinic/controllers/VetController.java
@@ -1,22 +1,28 @@
 package com.jmoe.petclinic.controllers;
 
+import com.jmoe.petclinic.model.Vet;
 import com.jmoe.petclinic.services.VetService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
 
-    @GetMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"/vets", "/vets/", "/vets/index", "/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVets() {
+        return vetService.findAll();
     }
 }
